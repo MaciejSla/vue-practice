ff02445dcb1124f95333e266323ee7950045ea3b
Maciej Sładkiewicz <m.sladkiewicz.633@studms.ug.edu.pl>
2024-05-23 21:50:20 +0200

    Add text to carousel images

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,6 +6,10 @@ import {
   // CarouselNext,
   // CarouselPrevious
 } from '@/components/ui/carousel'
+
+const bannerText = ['We Belive In Humanity', 'A Powerful Religion Theme', "It's Useless To Lecture"]
+const bannerSubtext =
+  'Enthusiastically underwhelm quality benefits rather than professional "outside the box" thinking. Distinctively network highly efficient leadership skills'
 </script>
 
 <template>
@@ -13,7 +17,21 @@ import {
   <Carousel :opts="{ loop: true }">
     <CarouselContent>
       <CarouselItem v-for="(_, index) in 3" :key="index">
-        <img :src="`/src/assets/images/banner/0${index + 1}.jpg`" />
+        <div class="relative flex select-none flex-col items-center justify-center text-white">
+          <div class="absolute flex w-[35rem] flex-col gap-4">
+            <h1 class="font-serif text-6xl font-semibold">{{ bannerText[index] }}</h1>
+            <p class="font-medium">{{ bannerSubtext }}</p>
+            <button
+              class="transition-color w-fit rounded-full bg-yellow-500 px-6 py-4 font-bold text-white duration-500 hover:bg-[#da5455]"
+            >
+              GET HELP NOW
+            </button>
+          </div>
+          <img
+            :src="`/src/assets/images/banner/0${index + 1}.jpg`"
+            class="h-[46.5rem] w-full object-cover"
+          />
+        </div>
       </CarouselItem>
     </CarouselContent>
   </Carousel>
