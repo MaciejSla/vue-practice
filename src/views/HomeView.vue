8cf3dbea6e3c6c57c1cea56a71afc25bbc54e6c9
Maciej Sładkiewicz <m.sladkiewicz.633@studms.ug.edu.pl>
2024-05-23 22:20:23 +0200

    Add autoplay to carousel, scroll to top and explicitly import images

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,27 @@
 <script setup>
 import { Carousel, CarouselContent, CarouselItem } from '@/components/ui/carousel'
+import Autoplay from 'embla-carousel-autoplay'
+
+import img1 from '@/assets/images/banner/01.jpg'
+import img2 from '@/assets/images/banner/02.jpg'
+import img3 from '@/assets/images/banner/03.jpg'
 
 const bannerText = ['We Belive In Humanity', 'A Powerful Religion Theme', "It's Useless To Lecture"]
 const bannerSubtext =
   'Enthusiastically underwhelm quality benefits rather than professional "outside the box" thinking. Distinctively network highly efficient leadership skills'
+
+const bannerBg = [img1, img2, img3]
+
+const scrollToTop = () => {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  })
+}
 </script>
 
 <template>
-  <Carousel :opts="{ loop: true }">
+  <Carousel :opts="{ loop: true }" :plugins="[Autoplay({ delay: 10000 })]" class="bg-black">
     <CarouselContent>
       <CarouselItem v-for="(_, index) in 3" :key="index">
         <div class="relative flex select-none flex-col items-center justify-center text-white">
@@ -15,17 +29,16 @@ const bannerSubtext =
             <h1 class="font-serif text-6xl font-semibold">{{ bannerText[index] }}</h1>
             <p class="font-medium">{{ bannerSubtext }}</p>
             <button
-              class="transition-color w-fit rounded-full bg-yellow-500 px-6 py-4 font-bold text-white duration-500 hover:bg-[#da5455]"
+              @click="scrollToTop"
+              class="transition-color w-fit rounded-full bg-yellow-500 px-6 py-4 font-bold text-white duration-300 hover:bg-[#da5455]"
             >
               GET HELP NOW
             </button>
           </div>
-          <img
-            :src="`/src/assets/images/banner/0${index + 1}.jpg`"
-            class="h-[46.5rem] w-full object-cover"
-          />
+          <img :src="bannerBg[index]" class="h-[46.5rem] w-full object-cover" />
         </div>
       </CarouselItem>
     </CarouselContent>
   </Carousel>
+  <div class="h-screen bg-gray-500"></div>
 </template>
