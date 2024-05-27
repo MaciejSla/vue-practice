36dd3bb90d612585f900cf969f71349eeb6864e9
Maciej Sładkiewicz <m.sladkiewicz.633@studms.ug.edu.pl>
2024-05-27 19:29:28 +0200

    Add on slide change animations

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,7 @@
 import { Carousel, CarouselContent, CarouselItem } from '@/components/ui/carousel'
 import Autoplay from 'embla-carousel-autoplay'
 import { scrollToTop } from '@/lib/utils'
+import { ref, nextTick, watch } from 'vue'
 
 import img1 from '@/assets/images/banner/01.jpg'
 import img2 from '@/assets/images/banner/02.jpg'
@@ -13,22 +14,61 @@ const bannerSubtext =
 
 const bannerBg = [img1, img2, img3]
 
+const api = ref()
+const currentSlide = ref(0)
+
+function setApi(val) {
+  api.value = val
+}
+
+const stop = watch(api, (api) => {
+  if (!api) return
+
+  nextTick(() => {
+    stop()
+  })
+
+  api.on('select', (test) => {
+    console.log(test.selectedScrollSnap())
+    currentSlide.value = test.selectedScrollSnap()
+  })
+})
 </script>
 
 <template>
-  <Carousel :opts="{ loop: true }" :plugins="[Autoplay({ delay: 10000 })]" class="bg-black">
+  <Carousel
+    :opts="{ loop: true }"
+    :plugins="[Autoplay({ delay: 10000 })]"
+    class="bg-black"
+    @init-api="setApi"
+  >
     <CarouselContent>
-      <CarouselItem v-for="(_, index) in 3" :key="index">
-        <div class="relative flex select-none flex-col items-center justify-center text-white">
-          <div class="absolute flex w-[35rem] flex-col gap-4">
-            <h1 class="font-serif text-6xl font-semibold">{{ bannerText[index] }}</h1>
-            <p class="font-medium">{{ bannerSubtext }}</p>
-            <button
-              @click="scrollToTop"
-              class="transition-color w-fit rounded-full bg-yellow-500 px-6 py-4 font-bold text-white duration-300 hover:bg-[#da5455]"
-            >
-              GET HELP NOW
-            </button>
+      <CarouselItem v-for="(_, index) in 3" :key="index" class="basis-1/1">
+        <div class="relative flex select-none items-center text-white">
+          <div class="absolute flex w-full justify-around">
+            <div v-if="index != 1"></div>
+            <div class="flex w-[35rem] flex-col gap-4">
+              <h1
+                :class="`font-serif text-6xl font-semibold transition-all duration-300 ease-in-out ${index == currentSlide ? 'slide-up' : 'translate-y-[200%] opacity-0'}`"
+                :key="`${index}-title`"
+              >
+                {{ bannerText[index] }}
+              </h1>
+              <p
+                :class="`font-medium transition-all duration-300 ease-in-out ${index == currentSlide ? 'slide-up' : 'translate-y-[200%] opacity-0'}`"
+                :key="`${index}-subtext`"
+              >
+                {{ bannerSubtext }}
+              </p>
+              <button
+                @click="scrollToTop"
+                :class="`w-fit rounded-full bg-yellow-500 px-6 py-4 font-bold text-white transition-all duration-300 ease-in-out hover:bg-[#da5455] ${index == currentSlide ? 'slide-up' : 'translate-y-[200%] opacity-0'}`"
+                :key="`${index}-button`"
+              >
+                GET HELP NOW
+              </button>
+            </div>
+            <div v-if="index == 1"></div>
           </div>
           <img :src="bannerBg[index]" class="h-[46.5rem] w-full object-cover" />
         </div>
@@ -37,3 +77,20 @@ const bannerBg = [img1, img2, img3]
   </Carousel>
   <div class="h-screen bg-gray-500"></div>
 </template>
+
+<style scoped>
+.slide-up {
+  animation: slide-up 0.7s ease-in-out;
+}
+
+@keyframes slide-up {
+  0% {
+    transform: translateY(200%);
+    opacity: 0;
+  }
+  100% {
+    transform: translateY(0);
+    opacity: 1;
+  }
+}
+</style>
