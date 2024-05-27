942d8a2241ca721ac21c9c9cd7b0d0b0d77012b1
Maciej Sładkiewicz <m.sladkiewicz.633@studms.ug.edu.pl>
2024-05-27 15:23:11 +0200

    Add fake loading animation

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,39 @@
 import { RouterView } from 'vue-router'
 import TopHeader from '@/components/TopHeader.vue'
 import NavigationBar from '@/components/NavigationBar.vue'
+import { onMounted, ref } from 'vue';
+
+const isLoading = ref(true)
+
+// maybe useful later?
+onMounted(() => {
+  // isLoading.value = false
+  setTimeout(() => {
+    isLoading.value = false
+  }, 1000)
+})
 </script>
 
 <template>
-  <TopHeader />
-  <NavigationBar />
-  <RouterView />
+  <Transition name="fade">
+    <div class="flex flex-col h-screen w-full bg-yellow-500 absolute" v-if="isLoading"></div>
+    <div v-else class="absolute w-full">
+      <TopHeader />
+      <NavigationBar />
+      <RouterView />
+    </div>
+  </Transition>
 </template>
+
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s ease-out;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0
+}
+
+</style>
