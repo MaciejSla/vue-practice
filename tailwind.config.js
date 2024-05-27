5ab192cf528f98657e5e4233f522ec5077d9d53d
Maciej Sładkiewicz <m.sladkiewicz.633@studms.ug.edu.pl>
2024-05-27 18:14:00 +0200

    Add custom fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -32,8 +32,12 @@ export default {
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out'
+      },
+      fontFamily: {
+        sans: ['Montserrat', 'sans-serif'],
+        serif: ['Playfair Display', 'serif'],
       }
-    }
+    },
   },
   plugins: [animate]
 }
