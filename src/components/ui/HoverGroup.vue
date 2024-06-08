daf724e5c0cc7964ffce05b630d954e49eba2dd3
Maciej Sładkiewicz <m.sladkiewicz.633@studms.ug.edu.pl>
2024-06-08 17:53:18 +0200

    Fix quotes

--- src/components/ui/HoverGroup.vue
+++ src/components/ui/HoverGroup.vue
@@ -12,18 +12,18 @@ const { title, subtext } = defineProps({
 </script>
 
 <template>
-  <div :class="`group flex items-center gap-8 xs:p-4`">
+  <div class="group flex items-center gap-8 xs:p-4">
     <div
-      :class="`rounded-full border-none outline-amber-300 transition-all duration-300 group-hover:ring-2 group-hover:ring-amber-300 group-hover:ring-offset-[6px]`"
+      class="rounded-full border-none outline-amber-300 transition-all duration-300 group-hover:ring-2 group-hover:ring-amber-300 group-hover:ring-offset-[6px]"
     >
-      <div :class="`rounded-full border p-6 transition-all duration-300 group-hover:bg-amber-300`">
-        <div :class="`size-8 fill-gray-500 transition-all duration-300 group-hover:fill-white`">
+      <div class="rounded-full border p-6 transition-all duration-300 group-hover:bg-amber-300">
+        <div class="size-8 fill-gray-500 transition-all duration-300 group-hover:fill-white">
           <slot></slot>
         </div>
       </div>
     </div>
     <div class="flex max-w-[37rem] flex-col items-start gap-2">
-      <h2 :class="`font-serif text-3xl transition-all duration-300 group-hover:text-amber-300`">
+      <h2 class="font-serif text-3xl transition-all duration-300 group-hover:text-amber-300">
         {{ title }}
       </h2>
       <p class="text-gray-500">
