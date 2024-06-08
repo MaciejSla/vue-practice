0fedb5547d1954f5be47447e7ef2a033dfe322b5
Maciej Sładkiewicz <m.sladkiewicz.633@studms.ug.edu.pl>
2024-06-08 22:38:06 +0200

    Move component to seperate file

---
+++ src/components/home/HomeGallery.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { IconPlus } from '@/components/icons'
+import images from '@/assets/images/gallery'
+import AnimatedAccent from '@/components/ui/AnimatedAccent.vue'
+</script>
+
+<template>
+  <div class="flex flex-col items-center gap-10 p-4">
+    <div class="group/anim flex flex-col items-center justify-center gap-4">
+      <h1 class="font-serif text-5xl">Christian Festivals</h1>
+      <AnimatedAccent reverse />
+    </div>
+    <!-- TODO filtering and image modal -->
+    <div class="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
+      <div
+        v-for="(img, index) in images"
+        :key="index"
+        class="group relative flex items-center justify-center"
+      >
+        <img :src="img" alt="image" />
+        <div
+          class="absolute bottom-0 left-0 right-0 z-10 flex h-full scale-0 items-center justify-center bg-black/60 text-white duration-300 group-hover:scale-100"
+        >
+          <div
+            class="hover:bg-main hover:border-main flex cursor-pointer items-center justify-center rounded-full border p-2 transition-colors duration-300"
+          >
+            <IconPlus class="size-5 stroke-white" stroke-width="4" />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,9 +2,7 @@
 import EventCounter from '@/components/home/EventCounter.vue'
 import HomeBanner from '@/components/home/HomeBanner.vue'
 import HomeAbout from '@/components/home/HomeAbout.vue'
-import AnimatedAccent from '@/components/ui/AnimatedAccent.vue'
-import images from '@/assets/images/gallery'
-import { IconPlus } from '@/components/icons'
+import HomeGallery from '@/components/home/HomeGallery.vue'
 </script>
 
 <template>
@@ -20,30 +18,6 @@ import { IconPlus } from '@/components/icons'
     </button>
   </div>
   <HomeAbout />
-  <div class="flex flex-col items-center gap-10 p-4">
-    <div class="group/anim flex flex-col items-center justify-center gap-4">
-      <h1 class="font-serif text-5xl">Christian Festivals</h1>
-      <AnimatedAccent reverse />
-    </div>
-    <!-- TODO filtering and image modal -->
-    <div class="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
-      <div
-        v-for="(img, index) in images"
-        :key="index"
-        class="group relative flex items-center justify-center"
-      >
-        <img :src="img" alt="image" />
-        <div
-          class="absolute bottom-0 left-0 right-0 z-10 flex h-full scale-0 items-center justify-center bg-black/60 text-white duration-300 group-hover:scale-100"
-        >
-          <div
-            class="hover:bg-main hover:border-main flex cursor-pointer items-center justify-center rounded-full border p-2 transition-colors duration-300"
-          >
-            <IconPlus class="size-5 stroke-white" stroke-width="4" />
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
+  <HomeGallery />
   <div class="h-screen bg-gray-500"></div>
 </template>
