eb13aba50975c6d48f24bc640f721b2bdb49f0f2
Maciej Sładkiewicz <m.sladkiewicz.633@studms.ug.edu.pl>
2024-06-29 20:17:37 +0200

    Add recent events

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,21 +3,23 @@ import EventCounter from '@/components/home/EventCounter.vue'
 import HomeBanner from '@/components/home/HomeBanner.vue'
 import HomeAbout from '@/components/home/HomeAbout.vue'
 import HomeGallery from '@/components/home/HomeGallery.vue'
+import HomeEvents from '@/components/home/HomeEvents.vue'
 </script>
 
 <template>
   <HomeBanner />
   <div class="flex flex-col items-center justify-center gap-10 bg-stone-950 p-10 xl:flex-row">
-    <h1 class="text-main font-serif text-5xl">Upcoming Event</h1>
+    <h1 class="font-serif text-5xl text-main">Upcoming Event</h1>
     <EventCounter :date="new Date('2025-06-04')" />
     <button
       @click="scrollToTop"
-      class="bg-main w-fit rounded-full px-6 py-4 font-bold text-white transition-all duration-300 ease-in-out hover:bg-[#da5455]"
+      class="w-fit rounded-full bg-main px-6 py-4 font-bold text-white transition-all duration-300 ease-in-out hover:bg-[#da5455]"
     >
       ALL EVENTS
     </button>
   </div>
   <HomeAbout />
   <HomeGallery />
+  <HomeEvents />
   <div class="h-screen bg-gray-500"></div>
 </template>
