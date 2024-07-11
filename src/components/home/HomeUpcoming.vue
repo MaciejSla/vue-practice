97a0aac0fcb946338af843a64fad97d62f93c6ce
MaciejSla <m.sladkiewicz.633@studms.ug.edu.pl>
2024-07-11 20:45:01 +0200

    Move 'Upcoming Events' to seperate component

---
+++ src/components/home/HomeUpcoming.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import EventCounter from '@/components/home/EventCounter.vue'
+import CustomButton from '@/components/ui/CustomButton.vue'
+import { scrollToTop } from '@/lib/utils'
+</script>
+
+<template>
+  <div class="flex flex-col items-center justify-center gap-10 bg-stone-950 p-10 xl:flex-row">
+    <h1 class="font-serif text-5xl text-main">Upcoming Event</h1>
+    <EventCounter :date="new Date('2025-06-04')" />
+    <CustomButton @click="scrollToTop"> ALL EVENTS </CustomButton>
+  </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
-import EventCounter from '@/components/home/EventCounter.vue'
 import HomeBanner from '@/components/home/HomeBanner.vue'
+import HomeUpcoming from '@/components/home/HomeUpcoming.vue'
 import HomeAbout from '@/components/home/HomeAbout.vue'
 import HomeGallery from '@/components/home/HomeGallery.vue'
 import HomeEvents from '@/components/home/HomeEvents.vue'
@@ -11,21 +11,11 @@ import HomeNews from '@/components/home/HomeNews.vue'
 import HomeQuotes from '@/components/home/HomeQuotes.vue'
 import HomeSponsors from '@/components/home/HomeSponsors.vue'
 import ContactForm from '@/components/ContactForm.vue'
-import { scrollToTop } from '@/lib/utils'
 </script>
 
 <template>
   <HomeBanner />
-  <div class="flex flex-col items-center justify-center gap-10 bg-stone-950 p-10 xl:flex-row">
-    <h1 class="font-serif text-5xl text-main">Upcoming Event</h1>
-    <EventCounter :date="new Date('2025-06-04')" />
-    <button
-      @click="scrollToTop"
-      class="w-fit rounded-full bg-main px-6 py-4 font-bold text-white transition-all duration-300 ease-in-out hover:bg-[#da5455]"
-    >
-      ALL EVENTS
-    </button>
-  </div>
+  <HomeUpcoming />
   <HomeAbout />
   <HomeGallery />
   <HomeEvents />
