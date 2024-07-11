5d329de3cb9cacfa0553228f27a1d93145036b7f
MaciejSla <m.sladkiewicz.633@studms.ug.edu.pl>
2024-07-11 20:31:49 +0200

    Add custom button with half of template animation

---
+++ src/components/ui/CustomButton.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { cn } from '@/lib/utils'
+
+const props = defineProps<{
+  class?: string
+}>()
+</script>
+
+<template>
+  <button
+    :class="
+      cn(
+        'group relative w-fit overflow-hidden rounded-full bg-main px-6 py-4 font-bold text-white',
+        props.class
+      )
+    "
+  >
+    <div class="relative z-10 flex h-full w-full items-center justify-center">
+      <slot />
+    </div>
+    <div
+      ref="el"
+      :class="`absolute left-0 top-0 z-0 h-full w-full origin-bottom scale-y-0 bg-[#da5455] transition-all duration-300 ease-in-out group-hover:scale-y-100`"
+    ></div>
+  </button>
+</template>
