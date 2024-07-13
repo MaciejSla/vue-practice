dd2f42cbbc65998106b78d17c712e715612e693e
MaciejSla <m.sladkiewicz.633@studms.ug.edu.pl>
2024-07-13 18:30:09 +0200

    Add basic route breadcrumb

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <script setup lang="ts">
-import { RouterView } from 'vue-router'
+import { RouterView, useRoute } from 'vue-router'
 import NavigationBar from '@/components/navigation/NavigationBar.vue'
 import { onMounted, ref } from 'vue'
 import LoadingScreen from '@/components/LoadingScreen.vue'
 import Toaster from '@/components/ui/toast/Toaster.vue'
 import PageFooter from '@/components/navigation/footer/PageFooter.vue'
+import AppLink from '@/components/navigation/AppLink.vue'
 
 const isLoading = ref(true)
+
+const route = useRoute()
 
 // maybe useful later?
 onMounted(() => {
@@ -21,6 +24,14 @@ onMounted(() => {
   <LoadingScreen :is-loading="isLoading">
     <div class="absolute w-full">
       <NavigationBar />
+      <div class="flex items-center justify-between p-8" v-if="route.name != 'home'">
+        <h1 class="font-serif text-4xl capitalize">{{ route.name }}</h1>
+        <div class="flex items-center gap-2">
+          <AppLink to="/" class="transition-colors duration-300 hover:text-main">Home</AppLink>
+          /
+          <div class="capitalize">{{ route.name }}</div>
+        </div>
+      </div>
       <RouterView />
       <PageFooter />
     </div>
---
+++ src/views/ContactView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import ContactForm from '@/components/ContactForm.vue'
+import ContactMap from '@/components/ContactMap.vue'
+</script>
+
+<template>
+  <div>
+    <ContactForm />
+    <ContactMap />
+  </div>
+</template>
