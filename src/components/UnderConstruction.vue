2c272c200033626b10951f921782daf5e1430168
MaciejSla <m.sladkiewicz.633@studms.ug.edu.pl>
2024-07-19 19:45:43 +0200

    Tweak colors

--- src/components/UnderConstruction.vue
+++ src/components/UnderConstruction.vue
@@ -5,11 +5,11 @@ import { ConstructionIcon } from 'lucide-vue-next'
 <template>
   <div class="flex w-full items-center justify-center p-6">
     <div class="flex flex-col items-center justify-center gap-4 md:flex-row">
-      <ConstructionIcon class="size-20 stroke-yellow-500 xl:size-32" />
+      <ConstructionIcon class="size-20 stroke-main xl:size-32" />
       <h1 class="text-center text-3xl font-semibold 2xs:text-5xl xl:text-7xl">
         Under Construction
       </h1>
-      <ConstructionIcon class="size-20 stroke-yellow-500 xl:size-32" />
+      <ConstructionIcon class="size-20 stroke-main xl:size-32" />
     </div>
   </div>
 </template>
