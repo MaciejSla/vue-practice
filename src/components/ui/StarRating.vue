c26eb3324651c3d88cd89bb10773edcdf02864ff
MaciejSla <m.sladkiewicz.633@studms.ug.edu.pl>
2024-07-19 17:49:04 +0200

    Add interactive version of StarRating

--- src/components/ui/StarRating.vue
+++ src/components/ui/StarRating.vue
@@ -2,15 +2,25 @@
 import IconStar from '@/components/icons/IconStar.vue'
 import { cn } from '@/lib/utils'
 
-const props = withDefaults(defineProps<{ rating: number; max?: number; class?: string }>(), {
-  rating: 0,
-  max: 5,
-  class: ''
-})
+const emit = defineEmits(['update:rating'])
+
+const props = withDefaults(
+  defineProps<{ rating: number; max?: number; class?: string; interactive?: boolean }>(),
+  {
+    rating: 0,
+    max: 5,
+    class: '',
+    interactive: false
+  }
+)
+
+const changeRating = (score: number) => {
+  emit('update:rating', score)
+}
 </script>
 
 <template>
-  <div class="flex gap-1">
+  <div class="flex gap-1" v-if="!props.interactive">
     <IconStar
       v-for="index in props.max"
       :key="index"
@@ -18,6 +28,18 @@ const props = withDefaults(defineProps<{ rating: number; max?: number; class?: s
         cn('size-2', index - 0.5 <= props.rating ? 'fill-main' : 'fill-gray-400', props.class)
       "
       :type="index <= props.rating ? 'full' : index - 0.5 <= props.rating ? 'half' : 'empty'"
+    />
+  </div>
+  <div class="flex" v-else>
+    <div @click="changeRating(0)" class="w-2"></div>
+    <IconStar
+      v-for="index in props.max"
+      :key="index"
+      :class="
+        cn('mr-1 size-2', index - 0.5 <= props.rating ? 'fill-main' : 'fill-gray-400', props.class)
+      "
+      :type="index <= props.rating ? 'full' : index - 0.5 <= props.rating ? 'half' : 'empty'"
+      @click="changeRating(index)"
     />
   </div>
 </template>
